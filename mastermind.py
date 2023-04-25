ac6069ae5b76d34c94d0e9c23fdf308a82fc5245
ahugo <hugo.andriamampianina@ynov.com>
2023-04-25 16:20:01 +0200

    verif si couleur presente

--- mastermind.py
+++ mastermind.py
@@ -15,12 +15,23 @@ print(code)
 def userTurn():
     userTry = []
     for i in range(4): 
-        userColor = input("Choisir couleur : ")
+        userColor = input("Choisir couleur "+str(i+1)+" : ")
         userTry.append(userColor)
 
     if code == userTry:
-        print("yes")
+        print("Congratulations, you win")
     else:
-        print("no")
+        colorPos(userTry)
 
+def colorPos(userTry):
+    ColorIn = 0
+    ColorInGoodPos = 0
+    for i in range(len(userTry)):
+        if userTry[i] == code[i]:
+            ColorInGoodPos += 1
+        if userTry[i] in code:
+            ColorIn += 1
+    print("Vous avez "+str(ColorIn)+" couleurs présentes")
+    print("Vous avez "+str(ColorInGoodPos)+" couleurs bien placées")
+    
 userTurn()
